122b32ed046d7421af5a3a289ba98f3d8c868647
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2022-11-12 03:37:24 +0900

    resultvanilla count added

--- logicManResult.js
+++ logicManResult.js
@@ -1,5 +1,7 @@
 const resultVanilla = document.getElementById("result-vanilla")
 const resultShow = document.getElementById("result-show")
+const resultCount = document.getElementById("result-count")
+
 const backButton = document.querySelector(".back-button")
 
 
@@ -8,7 +10,9 @@ const resultRecord = JSON.parse(localStorage.getItem("resultArr"))
 averageResult()
 show()
 
+
 function averageResult() {
+    // 数字の項目を平均値に
     let heightArr = []
     let ageArr = []
     let salaryArr = []
@@ -21,9 +25,36 @@ function averageResult() {
     const averageAge = average(ageArr.map(Number))
     const averageSalary = average(salaryArr.map(Number))
 
-    resultVanilla.innerHTML = `<p>平均身長は${averageHeight}cm</p>
+    // 学歴を数える
+    let nitto = 0
+    let march = 0
+    let sokei = 0
+    let todai = 0
+    for(let i = 0; i < resultRecord.length; i++){
+      const school = resultRecord[i][3]
+      if(school === "日東駒専"){
+        nitto++
+      }else if(school === "MARCH"){
+        march++
+      }else if(school === "早慶"){
+        sokei++
+      }else if(school === "東大"){
+        todai++
+      }
+    }
+
+    resultVanilla.innerHTML = `
+    <div class="result-vanilla-1">
+    <p>平均身長は${averageHeight}cm</p>
     <p>平均年齢は${averageAge}歳</p>
-    <p>平均年収は${averageSalary}万円</p>`
+    <p>平均年収は${averageSalary}万円</p>
+    </div>
+    <div class="result-vanilla-2">
+    <p>東大: ${todai}</p>
+    <p>早慶: ${sokei}</p>
+    <p>MARCH: ${march}</p>
+    <p>日東駒専: ${nitto}</p>
+    </div>`
 }
 
 // 引数のarrayの平均を計算
@@ -40,6 +71,7 @@ function average(arr) {
 function show(){
   resultShow.innerHTML = `${resultRecord}`
 }
+
 
 
 
